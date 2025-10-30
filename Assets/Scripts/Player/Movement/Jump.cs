eac14436433a2c39fa56372609b6286088f3bcd7
Emanuel Dahlan <Dahlin.emanuel@gmail.com>
2025-10-30 15:09:16 +0100

    fixt the moment to be FixedUpdate insted of Update

--- Assets/Scripts/Player/Movement/Jump.cs
+++ Assets/Scripts/Player/Movement/Jump.cs
@@ -21,29 +21,22 @@ namespace Player.Movement
             _jumpPower = jumpPower;
             _isJump = false;
         }
-        public static void OnJumpUpdate()
+        public static void OnJumpUpdate() {}
+
+        public static void OnJumpFixedUpdate()
         {
-            if (_isJump == false) { _rigidbody2D.linearVelocity += Vector2.zero; return; }
-
-            // _rigidbody2D.linearVelocity += Vector2.up * (_jumpPower * Time.deltaTime);
-            _rigidbody2D.linearVelocity += Vector2.up * _jumpPower;
-            
+            if (_isJump == false) { _rigidbody2D.linearVelocityY += 0; return; }
+            _rigidbody2D.linearVelocityY += Vector2.up.y * _jumpPower;
             _isJump = false;
-
         }
-        public static void OnJumpFixedUpdate() {}
         
         public static void OnJump(bool jump)
         {
-            if (jump == false)
-            {
-                _isJump = false;
-                return;
-            }
+            if (jump == false) { _isJump = false; return; }
             
             if (!IsGrounded(_rigidbody2D))
             {
-                Debug.LogError("\"IsGrounded is False"); 
+                Debug.LogError("IsGrounded is False"); 
                 _isJump = false;
                 return;
             }
@@ -54,17 +47,11 @@ namespace Player.Movement
         public static bool IsGrounded(Rigidbody2D rigidbody2D)
         {
             const float groundedDistance = 3f;
-            // if (rigidbody2D.linearVelocityY <= 0)
-            // {
-            //     Debug.Log("IsGrounded failed because of linearVelocityY <= 0 and the value was Value: " + _rigidbody2D.linearVelocityY);
-            //     return false;
-            // }
-
             Vector2 position = rigidbody2D.transform.position;
 
             Debug.Log(LayerMask.NameToLayer("Ground"));
             Debug.DrawLine(position, new Vector2(position.x ,position.y + Vector2.down.y * groundedDistance), Color.black, 1000);
-            RaycastHit2D hit = Physics2D.Raycast(position, Vector2.down, groundedDistance, GroundLayerMask);
+            RaycastHit2D hit = Physics2D.Raycast(position, Vector2.down, groundedDistance);
             return hit.transform is not null;
         }
     }
--- Assets/Scripts/Player/Movement/Move.cs
+++ Assets/Scripts/Player/Movement/Move.cs
@@ -8,14 +8,19 @@ namespace Player.Movement
         private static Rigidbody2D _rigidbody2D;
         private static float _speed;
         private static float _moveDirection;
+        public static float MoveDirection => _moveDirection;
 
         public static void OnMovementAwake(Rigidbody2D rigidbody2D, float speed)
         {
             _rigidbody2D = rigidbody2D;
             _speed = speed;
         }
-        public static void OnMovementUpdate() => ApplyForces();
-        public static void OnMovementFixedUpdate() {}
+        public static void OnMovementUpdate() {}
+
+        public static void OnMovementFixedUpdate()
+        {
+            ApplyForces();
+        }
         
         public static void OnMove(float moveDirection)
         {
@@ -26,7 +31,7 @@ namespace Player.Movement
         private static void ApplyForces()
         {
             // Move Force
-            _rigidbody2D.linearVelocityX = _moveDirection * (_speed * Time.deltaTime);;
+            _rigidbody2D.linearVelocityX = _moveDirection * (_speed);
         }
     }
 }
