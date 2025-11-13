8b3c0a602f13503001b62ecef83d0e2fedd1a0be
Emanuel Dahlan <Dahlin.emanuel@gmail.com>
2025-11-13 12:26:00 +0100

    clind up my code bace

--- Assets/Scripts/Player/Movement/Jump.cs
+++ Assets/Scripts/Player/Movement/Jump.cs
@@ -2,6 +2,8 @@ using System.Runtime.CompilerServices;
 using Animation;
 using UnityEngine;
 
+using static Global.GlobalFunctionsLibrary;
+
 namespace Player.Movement
 {
     public static class Jump
@@ -53,29 +55,6 @@ namespace Player.Movement
             }
             GetIsJumping = true;
             AnimationsStateMachine.SetState(AnimationsStates.IsJumpingRight);
-        }
-        
-        public static bool IsGrounded(Rigidbody2D rigidbody2D)
-        {
-            const float groundedDistance = 2f;
-            int[] groundLayerMasks = new []
-            {
-                1<<3, // layer 3 in bits
-                1<<6 // layer 6 in bits
-            };
-            Vector2 position = rigidbody2D.position;
-            RaycastHit2D returnValue = new();
-            foreach (int groundLayerMask in groundLayerMasks)
-            {
-                RaycastHit2D hit = Physics2D.Raycast(position, Vector2.down, groundedDistance, groundLayerMask);
-                if (hit.transform is null) continue;
-                returnValue = hit;
-            }
-            #if UNITY_EDITOR
-            if (returnValue.transform is not null) 
-                Debug.DrawLine(position, new Vector2(position.x ,position.y + Vector2.down.y * groundedDistance), Color.black, 1000);
-            #endif 
-            return returnValue.transform is not null;
         }
     }
 }
