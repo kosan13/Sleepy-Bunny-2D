0ef7c73df75dbebbfbe6988ffe512e95f887a4e2
theohogben <theo@tyrantfilms.com>
2025-11-22 12:57:46 +0100

    paralax added
    paralax script added

---
+++ Assets/Scripts/Testing/Paralax/Paralax.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class SimpleParallax : MonoBehaviour
+{
+    [Header("Target (Camera or Player)")]
+    public Transform target;  // drag your Camera or Player here in Inspector
+
+    [Header("Parallax Amount")]
+    [Range(0f, 1f)]
+    public float parallaxSpeed = 0.5f;// 0 = no move, 1 = same as target
+
+    Vector3 startPos;
+    float startTargetX;
+    
+
+    void Start()
+    {
+        if (target == null)
+        {
+            target = Camera.main.transform; // fallback
+        }
+
+        startPos = transform.position;
+        startTargetX = target.position.x;
+    }
+
+    void LateUpdate()
+    {
+        float deltaX = (target.position.x - startTargetX) * parallaxSpeed;
+
+        transform.position = new Vector3(
+            startPos.x + deltaX,
+            startPos.y,
+            startPos.z
+        );
+    }
+}
