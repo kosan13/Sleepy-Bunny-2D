04c3ce38941b3f06cfb69def0f3837210340f46c
EnokNord <51075222+EnokNord@users.noreply.github.com>
2025-11-25 20:52:31 +0100

    Fixed prallex scroling down and upp

--- Assets/Scripts/Testing/Paralex scrolling/Rightparalex.cs
+++ Assets/Scripts/Testing/Paralex scrolling/Rightparalex.cs
@@ -1,73 +1,121 @@
+using System.Drawing;
 using UnityEngine;
+using UnityEngine.Device;
 
 public class RightParalex : MonoBehaviour
 {
-    private float lenght, startpos;
-    private GameObject Cam;
+    private float startposX;
+    private float startposY; 
+    private GameObject Player;
+    public GameObject Self;
+    private GameObject Top;
+    private GameObject Bottom;
 
-    public float paralexEffect;
+    private Renderer Render;
 
-    public GameObject rightside;
-    public GameObject middle;
-    public GameObject leftside;
+    public Camera Cam;
+
+    //is the foreground gorounded or not?
+    public bool CellingForeground = false;
+
+    private bool Stuck;
+    private float cacheY;
+
+    //How much the foreground should move
+    public float paralexEffect = 0.5f;
    
 
-    private bool rightTwice;
-    private bool leftTwice;
-
+  
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         GameObject player = GameObject.Find("player");
-        GameObject Right = GameObject.Find("Blured foreground 2");
-        GameObject Middle = GameObject.Find("Blured foreground 1");
-        GameObject Left = GameObject.Find("Blured foreground 3");
+        GameObject top = GameObject.Find("CamraTop");
+        GameObject bottom = GameObject.Find("CameraBottom");
 
-        Cam = player;
-        rightside = Right;
-        middle = Middle;
-       
+        Render = Self.GetComponent<Renderer>();
 
-        startpos = transform.position.x;
-        lenght = GetComponent<SpriteRenderer>().bounds.size.x;
-        rightside = GetComponent<GameObject>();
-        middle = GetComponent<GameObject>();
-       
+
+        Top = top;
+        Bottom = bottom;
+        Player = player;
+        
+        Cam = Camera.main;
+
+        startposX = transform.position.x;
+        startposY = transform.position.y;
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float temp = (Cam.transform.position.x * (1 - paralexEffect));
-        float dist = (Cam.transform.position.x * -paralexEffect);
+        //How much the foreground moves when moved by the paralexScrolling
+        float distx = (Player.transform.position.x * -paralexEffect);
+        float disty = (Player.transform.position.y * -paralexEffect);
 
-        transform.position = new Vector3(startpos + dist, transform.position.y, 0);
-
-       
-
-        // Does not work do not know why
-        /*
-        // Used to repeat infenetly
-        if (temp > startpos + lenght)
+        transform.position = new Vector3(startposX + distx, transform.position.y, 0);
+        if (CellingForeground)
         {
-            if (rightTwice)
+           if (Top.transform.position.y > Render.bounds.max.y)
+           {
+                Debug.Log("Too low");
+                if (!Stuck)
+                {
+                    cacheY = disty;
+                    Stuck = true;
+                }
+
+                else if (Stuck)
+                {
+                    if(disty > cacheY)
+                    {
+                        transform.position = new Vector3(transform.position.x, startposY + disty, 0);
+                        Stuck = false;
+                        transform.position = new Vector3(transform.position.x, Top.transform.position.y - (0.5f * Render.bounds.size.y), 0);
+                    }
+                }
+                return;
+           }
+
+                transform.position = new Vector3(transform.position.x, startposY + disty, 0);
+
+            
+        }
+
+        
+
+        else if (!CellingForeground)
+        {
+
+            if (Bottom.transform.position.y - 0.5f < Render.bounds.min.y)
             {
-                middle.transform.position = new Vector2(startpos += 2 * lenght, 0);
+                Debug.Log("Too high");
+                
+                if (!Stuck)
+                {
+                    Debug.Log("render" + Render.bounds.size.y);
+                    cacheY = disty;
+                    Stuck = true;
+                    transform.position = new Vector3(transform.position.x,Bottom.transform.position.y + (0.5f * Render.bounds.size.y), 0);
+                }
+
+                else if (Stuck)
+                {
+                    if (disty < cacheY)
+                    {
+                        transform.position = new Vector3(transform.position.x, startposY + disty, 0);
+                        Stuck = false;
+                    }
+                }
                 return;
             }
-            rightside.transform.position = new Vector2(startpos += 2 * lenght, 0);
+            Debug.Log(disty);
 
+            transform.position = new Vector3(transform.position.x, startposY + disty, 0);
         }
-        else if (temp < startpos - lenght)
-        {
-            if (leftTwice)
-            {
-                middle.transform.position = new Vector2(startpos += -2 * lenght, 0);
-                return;
-            }
-            rightside.transform.position = new Vector2(startpos -= 2 * lenght, 0);
-        }
-        */
+
+
 
     }
 }
